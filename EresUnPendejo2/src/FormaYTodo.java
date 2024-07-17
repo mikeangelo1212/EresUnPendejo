3bbad9c332f1ad56f65352aaa7c0042c8554e719
mikeangelo1212 <miguelmounstro@hotmail.com>
2024-07-17 15:56:04 -0500

    When sacas la 2.0 el mismo dia

--- EresUnPendejo2/src/FormaYTodo.java
+++ EresUnPendejo2/src/FormaYTodo.java
@@ -19,36 +19,61 @@ public class FormaYTodo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame frame = new JFrame();
-		frame.setSize(500,500);
-		frame.setLayout(new GridLayout(2,1,0,10));
-		
-		JButton boton = new JButton();
-		boton.setText("Averigualo");
-		
-		boton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Boton pendejo");
-				frame.setVisible(false);
-				JOptionPane.showMessageDialog(null,"Eres un pendejo","Respuesta",1,UIManager.getIcon("FileView.hardDriveIcon"));
-				frame.setVisible(true);
+//		JFrame frame = new JFrame();
+//		frame.setSize(500,500);
+//		frame.setLayout(new GridLayout(2,1,0,10));
+//		
+//		JButton boton = new JButton();
+//		boton.setText("Averigualo");
+//		
+//		boton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println("Boton pendejo");
+//				frame.setVisible(false);
+//				JOptionPane.showMessageDialog(null,"Eres un pendejo","Respuesta",1,UIManager.getIcon("FileView.hardDriveIcon"));
+//				frame.setVisible(true);
+//			}
+//		});
+//		
+//		JLabel label = new JLabel();
+//		label.setText("Eres un pendejo?");
+//		label.setHorizontalAlignment(JLabel.CENTER);
+//		label.setFont(new Font("Helvetica",Font.BOLD,30));
+//		
+//		
+//		
+//		frame.add(label);
+//		frame.add(boton);
+//		
+//		
+//		
+//		
+//		frame.setVisible(true);
+		boolean perro=true;
+		while(perro) {
+			int res= JOptionPane.showConfirmDialog(null, "Eres un pendejo?","Pregunta importante",JOptionPane.YES_NO_CANCEL_OPTION);
+			switch (res) {
+			case 0:
+				System.out.println("Yes");
+				JOptionPane.showMessageDialog(null, "Me alegro que lo sepas :)","Felicidades",JOptionPane.INFORMATION_MESSAGE);
+				perro=false;
+				break;
+	
+			case 1:
+				System.out.println("No");
+				JOptionPane.showMessageDialog(null, "Siguelo creyendo. Pendejo.","La dura verdad",JOptionPane.ERROR_MESSAGE);
+				break;
+			case 2:
+				System.out.println("Cancel");
+				JOptionPane.showMessageDialog(null, "A donde crees que vas? Responde la pregunta","Ni creas que te iras",JOptionPane.WARNING_MESSAGE);
+				break;
+			default:
+				System.out.println("App closed");
+				JOptionPane.showMessageDialog(null, "Para mi que eres marica","No seas culo",JOptionPane.WARNING_MESSAGE);
+				break;
 			}
-		});
-		
-		JLabel label = new JLabel();
-		label.setText("Eres un pendejo?");
-		label.setHorizontalAlignment(JLabel.CENTER);
-		label.setFont(new Font("Helvetica",Font.BOLD,30));
-		
-		
-		
-		frame.add(label);
-		frame.add(boton);
-		
-		
-		
-		
-		frame.setVisible(true);
+		}
+
 	}
 
 }
